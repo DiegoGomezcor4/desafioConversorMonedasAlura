a93f1df8f87fca3c6635a05e07c135504e8caddd
Diego Gomez <diegocor4@gmail.com>
2023-09-26 22:43:17 -0300

    agregada clase conversor de temperatura

---
+++ src/conversorTemperatura/functionTemperatura.java
@@ -0,0 +1,4 @@
+package conversorTemperatura;
+
+public class functionTemperatura {
+}
--- src/principal/principal.java
+++ src/principal/principal.java
@@ -1,6 +1,7 @@
 package principal;
 
 import conversorMonedas.function;
+import conversorTemperatura.functionTemperatura;
 
 import javax.swing.*;
 
@@ -8,6 +9,7 @@ public class principal {
     public static void main(String[] args) {
 
         function monedas = new function();
+        functionTemperatura temperatura = new functionTemperatura();
 
         while(true){
             String opciones = (JOptionPane.showInputDialog(null, "Seleccione una opcion", "Menu",JOptionPane.QUESTION_MESSAGE,null,new Object[] {"Conversor de Moneda","Conversor de Temperatura"},"Seleccion")).toString();
